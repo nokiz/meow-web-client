76bd5681daad7132e8ec317ff994a61c2092a30b
Nando <hola@holasocnando.com>
2023-03-10 18:32:21 +0100

    Server IP change.

--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`http://172.17.0.4:3001/assets/${image}`}
       />
     </Box>
   );
