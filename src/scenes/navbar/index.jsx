0bf18a9d166941c7cebd89453ebd29662247d03d
Nando <hola@holasocnando.com>
2023-03-13 17:28:11 +0100

    Change name to CatPaws

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          CatPaws
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
